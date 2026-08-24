2a3945bd8d1e7124b1b09f003cc92c1fcd949a8e
Danila Spasov <spasov2000@mail.ru>
2026-08-24 19:03:30 +0300

    Add habit API and JWT authentication

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import HabitsPaginator
+from habits.serializers import HabitSerializer
+
+
+class HabitsViewSet(viewsets.ModelViewSet):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = HabitsPaginator
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user).order_by("id")
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PublicHabitsListAPIView(generics.ListAPIView):
+    queryset = Habit.objects.filter(is_public=True).order_by("id")
+    serializer_class = HabitSerializer
+    pagination_class = HabitsPaginator
+    permission_classes = (AllowAny,)
